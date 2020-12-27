94f27d57678713541cc70d7eb8aec40a082302b5
Shakib maansoori <shakibraza436@gmail.com>
2020-12-27 22:06:39 +0530

    initial commit

---
+++ app/src/main/java/com/shakibmark/dbdemo/params/Params.java
@@ -0,0 +1,15 @@
+package com.shakibmark.dbdemo.params;
+
+public class Params {
+
+    // Public Static Final Variables
+
+    public static final int DB_VERSION = 1 ;
+    public static final String DB_NAME = "contact_db";
+    public static final String TABLE_NAME = "contact_table";
+
+    // Keys Of our table in DB
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHONE = "phone_number";
+}

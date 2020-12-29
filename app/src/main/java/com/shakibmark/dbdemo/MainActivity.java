c9894acc7ca867711dc3c4099c923e292523dd2b
shakivhussain <shakibraza436@gmail.com>
2020-12-29 19:17:39 +0530

    add int

--- app/src/main/java/com/shakibmark/dbdemo/MainActivity.java
+++ app/src/main/java/com/shakibmark/dbdemo/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int i;
 
         MyDbHandler db = new MyDbHandler(MainActivity.this);
 
